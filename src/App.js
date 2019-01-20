54ff073c1dc41b383400ae4353134efb88794f81
DarryllRobinson <darryll@stillproud.com>
2019-01-20 13:55:00 +0200

    First steps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import "../node_modules/jquery/dist/jquery.min.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import './App.css';
 
 class App extends Component {
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
   render() {
+    const { isAuthenticated } = this.props.auth;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome to Radio Bingo!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          {
+            !isAuthenticated() && (
+              <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div  className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul  className="navbar-nav mr-auto">
+                    <li  className="nav-item active">
+                      <a  className="nav-link"
+                        onClick={this.login.bind(this)}
+                        style={{ cursor: "pointer" }}
+                      >
+                        Log In
+                      <span  className="sr-only">
+                      </span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+          )}
+          {
+            isAuthenticated() && (
+              <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div  className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul  className="navbar-nav mr-auto">
+                    <li  className="nav-item active">
+                      <a  className="nav-link"
+                        onClick={this.logout.bind(this)}
+                        style={{ cursor: "pointer" }}
+                      >
+                        Log Out
+                      <span  className="sr-only">
+                      </span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+          )}
+
         </header>
       </div>
     );
